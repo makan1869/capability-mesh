238b988f984d8c06fe6e19a364e3a2e18f07cbc9
Makan Sepehrifar <serenade@Makans-MacBook-Pro.local>
2026-04-10 18:37:20 +0200

    Initial Import

---
+++ src/main/java/nl/makan1869/capabilitymesh/CapabilityMeshApplication.java
@@ -0,0 +1,13 @@
+package nl.makan1869.capabilitymesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapabilityMeshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapabilityMeshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/makan1869/capabilitymesh/CapabilityMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.makan1869.capabilitymesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapabilityMeshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
