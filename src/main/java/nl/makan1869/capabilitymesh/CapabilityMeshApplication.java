449436c85761def34981fc5593b8b4d9350c3e8e
Makan Sepehrifar <makan.sepehrifar@rabobank.nl>
2026-04-15 14:36:40 +0200

    Cloned mostly from https://github.com/spring-ai-community/spring-ai-agent-utils/tree/main/examples/skills-demo to demo an agent in Action

--- src/main/java/nl/makan1869/capabilitymesh/CapabilityMeshApplication.java
+++ src/main/java/nl/makan1869/capabilitymesh/CapabilityMeshApplication.java
@@ -1,13 +1,74 @@
 package nl.makan1869.capabilitymesh;
 
+import org.springaicommunity.agent.tools.*;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.ToolCallAdvisor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class CapabilityMeshApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CapabilityMeshApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ChatClient.Builder chatClientBuilder,
+                                        @Value("${agent.skills.dirs:Unknown}") List<Resource> agentSkillsDirs, @Value("${mesh.brave.search.api-key}") String braveAPIKey) throws IOException {
+
+        return args -> {
+
+            ChatClient chatClient = chatClientBuilder // @formatter:off
+                    .defaultSystem("Always use the available skills to assist the user in their requests.")
+
+                    // Skills tool
+                    .defaultToolCallbacks(SkillsTool.builder().addSkillsResources(agentSkillsDirs).build())
+
+                    // Built-in tools
+                    .defaultTools(
+                            //Bash execution tool
+                            ShellTools.builder().build(),// built-in shell tools
+                            // Read, Write and Edit files tool
+                            FileSystemTools.builder().build(),// built-in file system tools
+                            // Smart web fetch tool
+                            SmartWebFetchTool.builder(chatClientBuilder.clone().build()).build(),
+                            // Brave web search tool
+                            BraveWebSearchTool.builder(braveAPIKey)
+                                    .resultCount(1).build())
+
+
+                    .defaultAdvisors(
+                            // Tool Calling advisor
+                            ToolCallAdvisor.builder().build(),
+                            // Custom logging advisor
+                            MyLoggingAdvisor.builder()
+                                    .showAvailableTools(false)
+                                    .showSystemMessage(false)
+                                    .build())
+                    .build();
+            // @formatter:on
+
+            var answer = chatClient
+                    .prompt("""
+					Explain reinforcement learning in simple terms and use.
+					Use required skills.
+					Then use the Youtube video https://youtu.be/vXtfdGphr3c?si=xy8U2Al_Um5vE4Jd transcript to support your answer.
+					Use absolute paths for the skills and scripts. Do not ask me for more details.
+					""")
+                    .call()
+                    .content();
+
+            System.out.println("The Answer: " + answer);
+        };
+
     }
 
 }
